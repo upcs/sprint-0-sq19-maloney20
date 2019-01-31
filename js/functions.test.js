1af169b9902d50b0506adaa7e166ba305d11e0e6
PatrickMaloney <maloney20@up.edu>
2019-01-30 16:10:11 -0800

    added a test that is expected to fail

--- js/functions.test.js
+++ js/functions.test.js
@@ -7,3 +7,7 @@ test('adds 1 + 2 to equal 3', () => {
 test('sum of 10 and 20 is defined', () => {
 	expect(functions.add(10, 20)).toBeDefined();
 });
+
+test('sum of 3 and 3 is 7', () => {
+	expect(functions.add(3, 3)).toBe(7);
+});
